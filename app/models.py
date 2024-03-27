0eeda51c525d41c22754e6a0d6e63f5da698061a
Lilibeth <lilibethocando@gmail.com>
2024-03-27 10:53:31 -0500

    Fix datetime in task table

--- app/models.py
+++ app/models.py
@@ -1,12 +1,12 @@
 from . import db
-from datetime import datetime
+from datetime import datetime, timezone
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
     completed = db.Column(db.Boolean)
-    date_created = db.Column(db.DateTime, default=datetime.now())
+    date_created = db.Column(db.DateTime, default=datetime.now(timezone.utc))
 
 
 # task = Task(title='First task', description='Finish the Flask Homework', completed=True)
