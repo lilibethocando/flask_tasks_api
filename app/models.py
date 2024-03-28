d2cd0bbf1d8d57c0386a168ebd4fc3a7700c424c
Lilibeth <lilibethocando@gmail.com>
2024-03-27 23:22:10 -0500

    Connect to Database

--- app/models.py
+++ app/models.py
@@ -6,7 +6,32 @@ class Task(db.Model):
     title = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
     completed = db.Column(db.Boolean)
-    date_created = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+    date_created = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
 
+
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.save()
+
+    def __repr__(self) -> str:
+        return f"<Task {self.id}|{self.title}>"
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "completed": self.completed,
+            "dateCreated": self.date_created
+            
+        }
 
 # task = Task(title='First task', description='Finish the Flask Homework', completed=True)
+    
+
+
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from flask import request, render_template
-from . import app
+from . import app, db
 from tasks import tasks_list
-from datetime import datetime
+from datetime import datetime, timezone
+from .models import Task
 
 
 @app.route("/")
@@ -11,16 +12,23 @@ def home():
 
 @app.route("/tasks")
 def get_tasks():
-    tasks = tasks_list
-    return render_template('tasks.html', tasks=tasks)
+    select_stmt = db.select(Task)
+    search = request.args.get('search')
+    print("Search parameter:", search)
+    if search:
+        select_stmt = select_stmt.where(Task.completed==search)
+        print("Generated SQL query:", str(select_stmt))
+    tasks = db.session.execute(select_stmt).scalars().all()
+    tasks_list = [task.to_dict() for task in tasks]
+    return render_template('tasks.html', tasks=tasks_list)
 
 @app.route("/tasks/<int:id>")
 def get_task(id):
-    tasks = tasks_list
+    tasks = db.session.execute(db.select(Task)).scalars().all()
     for task in tasks:
-        if task['id'] == id:
+        if task.id == id:
             return render_template('task.html', task=task)
-    return {'error': f"Task with id {id} does not exist"}, 400
+    return {'error': f"Task with id {id} does not exist"}, 404
 
 @app.route("/tasks", methods=['POST'])
 def create_task():
@@ -38,15 +46,7 @@ def create_task():
     title = data.get('title')
     description = data.get('description')
     completed = data.get('completed', False)
-    createdAt = datetime.now()
+    createdAt = datetime.now(timezone.utc)
 
-    new_task = {
-        'id': len(tasks_list) + 1,
-        'title': title,
-        'description': description,
-        'completed': completed,
-        'createdAt': createdAt
-    }
-
-    tasks_list.append(new_task)
-    return new_task, 201
+    new_task = Task(title=title, description=description, completed=completed)
+    return new_task.to_dict(), 201
